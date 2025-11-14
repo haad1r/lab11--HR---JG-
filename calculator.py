71df522c9cda2cc04fc2c7e87bb3264c0418bff5
juanj-gp <juanjgarcia2007@gmail.com>
2025-11-13 21:53:34 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,31 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
 
+def add(a, b):
+    return a+b
+
+
+def sub(a, b):
+    return a-b
+
+
+def mul(a, b):
+    return a*b
+
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Error: Cannot divide by 0.")
+    return b/a
+
+
+def log(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Error: Base a must be positibe but not 1.")
+    if b <= 0:
+        raise ValueError("Error: The value must be positive.")
+    return math.log(b, a)
+
+
+def exp(a, b):
+    return a**b
